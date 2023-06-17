f9db406305839cddd8aafaa550df370bdd8b8732
Samuel Fahrngruber <samuel.fahrngruber@gmail.com>
2023-06-17 22:23:45 +0200

    single page

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router';
-import HomeView from '../views/HomeView.vue';
+import PlayView from '../views/PlayView.vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -7,12 +7,7 @@ const router = createRouter({
     {
       path: '/',
       name: 'home',
-      component: HomeView,
-    },
-    {
-      path: '/play',
-      name: 'play',
-      component: () => import('../views/PlayView.vue'),
+      component: PlayView,
     },
   ],
 });
