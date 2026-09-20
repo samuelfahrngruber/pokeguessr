91d0b25eec4f060d2c52e417676606827e7d071d
Samuel Fahrngruber <samuel.fahrngruber-biernbaum@student.tugraz.at>
2026-09-20 09:42:00 +0200

    added missing component

---
+++ src/components/PokemonName.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+import type { Pokemon } from '@/stores/pokemon-list';
+import PokemonIcon from './PokemonIcon.vue';
+
+defineProps<{
+  pokemon: Pokemon;
+}>();
+</script>
+
+<template>
+  <span class="pokemon-name">
+    <span>{{ pokemon.name }}</span>
+    <PokemonIcon class="sprite" :pokemon="pokemon" aria-hidden="true" />
+  </span>
+</template>
+
+<style scoped>
+.pokemon-name {
+  display: inline-block;
+  position: relative;
+  padding-right: calc(40px + 0.25rem);
+}
+
+.sprite {
+  position: absolute;
+  right: 0;
+  top: 50%;
+  transform: translateY(-50%);
+}
+</style>
